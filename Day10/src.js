b01ef755e2a0bb797c2a60955e19dec6b40f60a2
optimist2309 <harishtiwary46@gmail.com>
2025-08-24 00:34:45 +0530

    ajax

---
+++ Day10/src.js
@@ -0,0 +1,8 @@
+const text_change = document.getElementById("testMagic");
+text_change.innerText = "Learning Javascript is fun !";
+alert("Text has been changed successfully!");
+text_change.style.color = "red";
+text_change.style.backgroundColor = "rebeccapurple";
+text_change.style.textAlign = "center";
+text_change.style.padding = "20px";
+text_change.style.borderRadius = "10px";
